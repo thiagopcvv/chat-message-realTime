718118593c31b96b953c8cbb8f7ef7be3c78de3e
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-09-23 17:00:53 -0400

    enchantment: alterando objeto de mensagens

--- app/(authenticated)/(chat)/[id]/utils/handleDataFunctionsChat.ts
+++ app/(authenticated)/(chat)/[id]/utils/handleDataFunctionsChat.ts
@@ -1,14 +1,13 @@
 const formatMessages = (messagesArray: any[], isUser: boolean) => {
+    console.log(messagesArray, "array")
     return messagesArray.map((message: any) => ({
         _id: message.id,  // ID único da mensagem
-        text: message.texto,  // Texto da mensagem
-        createdAt: new Date(message.data),  // Data de criação
+        text: message.mensagem,  // Texto da mensagem
+        createdAt: new Date(message.updated_at),  // Data de criação
         user: {
-            _id: isUser ? 1 : 2,  // ID do usuário (diferente para user e friend)
+            _id: message.user_id,
             name: isUser ? "Você" : "Amigo",
-            avatar: isUser
-                ? "https://placeimg.com/140/140/people"
-                : "https://placeimg.com/140/140/any",  // Avatar
+            avatar: null
         },
     }));
 };
